eb0a4a7cdc5a76d01701e19598f0be501b08baaa
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-03 12:04:34 -0300

    Delete implementado

--- 27-crud/php-action/delete.php
+++ 27-crud/php-action/delete.php
@@ -7,7 +7,7 @@ require_once 'db_connect.php';//conecta com o banco
 if (isset($_POST['btn-deletar'])){//verifica se o botao de deletar foi clicado e envia pelo mysqli_escape_string os parametros
     $id = mysqli_escape_string($connect, $_POST['id']);
 
-    $sql = "DELETE FROM clientes WHERE id='$id";
+    $sql = "DELETE FROM clientes WHERE id='$id'";
 
     if(mysqli_query($connect, $sql)){
         $_SESSION ['mensagem']= "Cliente Excluído!";
