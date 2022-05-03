95d54c9842ff37c13d64832a37354895868221a9
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-03 12:40:46 -0300

    segurança
    Tratamento para evitar inserção de scripts

--- 27-crud/php-action/create.php
+++ 27-crud/php-action/create.php
@@ -4,11 +4,18 @@ session_start();
 //Página para criar cliente
 require_once 'db_connect.php';//conecta com o banco
 
+function clear ($input){//função para evitar input de códigos
+    global $connect;//torna o $connect global
+    $var = mysqli_escape_string($connect, $input); //passa pelo escape string p evitar sql
+    $var = htmlspecialchars($var);//passa pelo specialchats p evitar xss
+    return $var;
+}
+
 if (isset($_POST['btn-cadastrar'])){//verifica se o botao de cadastro foi clicado e envia pelo mysqli_escape_string os parametros
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear ($_POST['email']);
+    $idade = clear ($_POST['idade']);
 
     $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')" ;
 
--- 27-crud/php-action/update.php
+++ 27-crud/php-action/update.php
@@ -4,12 +4,19 @@ session_start();
 //Página para criar cliente
 require_once 'db_connect.php';//conecta com o banco
 
+function clear ($input){//função para evitar input de códigos
+    global $connect;//torna o $connect global
+    $var = mysqli_escape_string($connect, $input); //passa pelo escape string p evitar sql
+    $var = htmlspecialchars($var);//passa pelo specialchats p evitar xss
+    return $var;
+}
+
 if (isset($_POST['btn-editar'])){//verifica se o botao de cadastro foi clicado e envia pelo mysqli_escape_string os parametros
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
-    $id = mysqli_escape_string($connect, $_POST['id']);
+    $nome = clear ($_POST['nome']);
+    $sobrenome = clear ($_POST['sobrenome']);
+    $email = clear ($_POST['email']);
+    $idade = clear ($_POST['idade']);
+    $id = clear ($_POST['id']);
 
     $sql = "UPDATE clientes SET nome = '$nome', sobrenome = '$sobrenome', email = '$email', idade = '$idade' WHERE id = '$id'";
 
